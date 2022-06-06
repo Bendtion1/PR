acc15222adb1cdd6bd8d36aa48f257705ae369ae
unknown <wangbosheng@yandex.ru>
2022-06-06 17:46:07 +0500

    Initial

--- placeremember/placeremember/settings.py
+++ placeremember/placeremember/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from telnetlib import Telnet
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
